6f75302ad63cd06ef7e0b19f17f1a47923652e65
Simon Struck <simonstruck70@gmail.com>
2017-03-16 10:12:14 +0100

    Tested and fixed Serialisation

--- src/protocol/RoutingEntry.java
+++ src/protocol/RoutingEntry.java
@@ -1,10 +1,17 @@
 package protocol;
 
+import java.io.Serializable;
+
 /**
  * Simple object which describes a route entry in the forwarding table.
  * Can be extended to include additional data.
  */
-public class RoutingEntry {
+public class RoutingEntry implements Serializable{
     public int nextHop;
     public int cost;
+
+    public RoutingEntry(int nextHop, int cost) {
+        this.nextHop = nextHop;
+        this.cost = cost;
+    }
 }
