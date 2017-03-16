c98e49df3918c60654de4ac1e0907e72d1bd32b5
Simon Struck <simonstruck70@gmail.com>
2017-03-16 09:29:25 +0100

    Created custom class

--- src/protocol/Program.java
+++ src/protocol/Program.java
@@ -12,14 +12,14 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Program {
     // Change to your group number (e.g. use a student number)
-    private static int groupId = 0;
+    private static int groupId = 1802607;
 
     // Change to your group password (doesn't matter what it is,
     // as long as everyone in the group uses the same string)
     private static String password = "Lamas is the password";
 
     // Change to your protocol implementation
-    private static Class<? extends IRoutingProtocol> protocolImpl = DummyRoutingProtocol.class;
+    private static Class<? extends IRoutingProtocol> protocolImpl = DistanceVectorProtocol.class;
 
     // Challenge server address
     private static String serverAddress = "netsys.student.utwente.nl";
